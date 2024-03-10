c51e262519f736779e44cc99cbfdb9e05d40b05f
山下永遠 <yamashita@private-laptop.local>
2024-03-10 21:07:41 +0900

    Initial commit

---
+++ test/cdk-migrate-practice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMigratePractice from '../lib/cdk-migrate-practice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-migrate-practice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMigratePractice.CdkMigratePracticeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
